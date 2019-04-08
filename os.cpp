1d03936acbf76986070e1bd28110975149996c75
Ravneet Kaur <rkloyal1122@gmail.com>
2019-04-08 21:07:16 +0530

    final commit

--- os.cpp
+++ os.cpp
@@ -76,3 +76,74 @@ bool Safe(int processes[], int avail[], int max[][R],
 			} 
 		} 
 
+// if there isn't a process in the safe state 
+		if (found == false) 
+		{ 
+			cout << "System is not in safe state"; 
+			return false; 
+		} 
+	} 
+
+	// If system is in safe state then 
+	// safe sequence will be calculated and printed
+	cout << "System is in safe state.\nSafe"
+		" sequence is: "; 
+	for (int i = 0; i < P ; i++) 
+		cout << safeSeq[i] << " "; 
+
+	return true; 
+} 
+
+// main code 
+int main() 
+{ 
+	int processes[] = {0, 1, 2}; 
+
+	// Available instances of resources 
+	int avail[] = {3, 3, 2}; 
+
+	// Maximum Resources that can be allocated to processes 
+	int max[][R] = {{8, 4, 3}, 
+					{6, 2, 0}, 
+					{3, 3, 3}, 
+					}; //as given in the *problem statement
+	//making arrays to ask the user to enter  the number of resources he/she needs
+	int reqP0[3]={}; //for process 0, P0
+	int reqP1[3]={};//for process 1, P1
+	int reqP2[3]={}; //for process 2, P2
+	//loop to take the input from the user and save it in arrays
+	for(int i=0;i<3;i++)
+	{
+		if(i==0)
+		{
+		for(int j=0;j<3;j++)
+		{
+			cout<<"Enter the resources needed for P0: ";
+			cin>>reqP0[j];
+		}
+		}
+		else if(i==1)
+		{
+		for(int j=0;j<3;j++)
+		{
+			cout<<"Enter the resources needed for P1: ";
+			cin>>reqP1[j];
+		}
+		}
+		else
+		{
+		for(int j=0;j<3;j++)
+		{
+			cout<<"Enter the resources needed for P2: ";
+			cin>>reqP2[j];
+		}
+		}
+	}
+	// Resources allocated to processes 
+	int allot[][R] = {reqP0[3], reqP1[3], reqP2[3]}; 
+
+	// Check if the system is in safe state or not 
+	Safe(processes, avail, max, allot); 
+
+	return 0; 
+} 
